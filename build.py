0701ffbbcc7e2a747981ed24c91eb7cc8b12a47c
Nymeria <morgan-durand@laposte.net>
2021-12-05 23:13:31 +0100

    test ofuscation

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 base = None
 executables = [Executable("main.py", base=base)]
-packages = ["idna", "pynput", "logging", "mss.tools", "time", "tkinter", "cv2", "numpy", "threading"]
+packages = ["idna", "pynput", "logging", "mss.tools", "time", "tkinter", "cv2", "numpy","pytransform", "threading"]
 options = {
     'build_exe': {
         'packages': packages,
