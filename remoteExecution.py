5e19dae9b5bbcfd2189ef004a928399036ecb76d
Nymerea <morgan-durand@laposte.net>
2021-12-08 10:26:36 +0100

    remove useless stuff

--- remoteExecution.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.get("https://raw.githubusercontent.com/Nymerea/aptima/master/instruction.py")
-script = r.text
-if script != "pass":
-    eval(compile(script, '<string>', 'exec'))
-    #exec(script)
-    pass
